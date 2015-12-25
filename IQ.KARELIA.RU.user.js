ce2ecd421feb5b746a76ff40daa71e516c347f6f
Aleksei <alexproinc@gmail.com>
2015-12-25 18:56:30 +0300

    Add question list

--- IQ.KARELIA.RU.user.js
+++ IQ.KARELIA.RU.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IQ.KARELIA.RU
 // @namespace    http://iq.karelia.ru/
-// @version      0.41
+// @version      0.5
 // @description  try to take over the world!
 // @author       Aleksei Turcevich
 // @match        http://iq.karelia.ru/next1.php
@@ -9,6 +9,8 @@
 // @resource     new_logo https://raw.githubusercontent.com/alexproinc/iq.karelia.ru/master/iq_large.gif
 // @grant        GM_getResourceURL
 // @grant        GM_addStyle
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @source       https://github.com/alexproinc/iq.karelia.ru/
 // @updateURL    https://github.com/alexproinc/iq.karelia.ru/raw/master/IQ.KARELIA.RU.user.js
 // @downloadURL  https://github.com/alexproinc/iq.karelia.ru/raw/master/IQ.KARELIA.RU.user.js
@@ -34,9 +36,14 @@ $(document).ready(function() {
     var questionTable = $("table[bgcolor='#FAF3FF']");
     var questionDiv = questionTable.find('div');
     var questionTables = questionDiv.find('table');
+    var questionState = $('font[color="#4040D0"]');
+    var state = { current: 0, all: 0};
     var questionText = "";
+    var center = $('center').first();
+    
     
     GM_addStyle(".q {background: white; padding: 10px 30px; border: #faf3ff solid 3px;}");
+    GM_addStyle(".qlist {background: cornsilk; text-align: left; width: 80%; margin: 20px; padding: 10px;}");
     $("img[height='90']").attr("src", GM_getResourceURL("new_logo"));
     questionDiv.css("background", "lightgray");
 
@@ -49,6 +56,21 @@ $(document).ready(function() {
         });
         questionText += crosslines(text) + " ";
     });
-
+    
+    var questionStateA = questionState.children("b").text().split('\u00A0');
+    if (questionStateA.length > 3)
+    {
+        state.current = questionStateA[1];
+        state.all = questionStateA[3];
+        //questionState.text(state.current + ' ' + state.all);
+        GM_setValue(state.current, questionText);
+        center.append('<div class="qlist"><ol></ol></div>');
+        for (var i = 0; i<state.current; i++)
+        {
+            center.find('ol').append("<li>" + GM_getValue(i, 'none o_O') + "</li>");
+        }
+    }
+    
     questionTable.append('<div class="q">'+questionText+'</div>');
+
 });
